ea07f4a7f12f0569925edcee64aebdcfb126b7ed
johnbitencourtbr <43508382+johnbitencourtbr@users.noreply.github.com>
2018-10-17 19:08:18 -0300

    Edit-Update

--- resources/views/cidades/edit.blade.php
+++ resources/views/cidades/edit.blade.php
@@ -8,4 +8,53 @@
 
 @section('content')
     <p>You are logged in!</p>
+<hr>
+
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+<div class="card uper">
+  <div class="card-header">
+    Edit Share
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('cidades.update', $cidades->id) }}">
+        @method('PATCH')
+        @csrf
+        <div class="form-group">
+          <label for="cidade">Cidade:</label>
+          <input type="text" class="form-control" name="cidade" value={{ $cidades->cidade }} />
+        </div>
+        <div class="form-group">
+          <label for="estados">Estados :</label>
+         {!!
+            
+            Form::select(
+                'estado',
+                $estados->pluck('nomeuf','id'),
+                old('estado') ?? $estadoSelecionado,
+                ['class' => 'form-control'],[ 'value' =>idestados]
+            )
+        !!}
+        
+        <button type="submit" class="btn btn-primary">Atualizar</button>
+      </form>
+  </div>
+</div>
+
+
+
+
+
 @stop
--- resources/views/cidades/index.blade.php
+++ resources/views/cidades/index.blade.php
@@ -8,4 +8,46 @@
 
 @section('content')
     <p>You are logged in!</p>
+   <hr>
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+<div class="uper">
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div><br />
+  @endif
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Cidade</td>
+          <td>Codigo do Estado</td>
+         
+          <td colspan="2">Ações</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($cidades as $cidades)
+        <tr>
+            <td>{{$cidades->id}}</td>
+            <td>{{$cidades->cidade}}</td>
+            <td>{{$cidades->idestados}}</td>
+         <td><a href="{{ route('cidades.edit',$cidades->id)}}" class="btn btn-primary">Editar</a></td>
+            <td>
+                <form action="{{ route('cidades.destroy', $cidades->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Excluir</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+
 @stop
