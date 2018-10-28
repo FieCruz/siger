b0d29045e28cf6c3bd490cae8a9cca5540ab2711
johnbitencourtbr <43508382+johnbitencourtbr@users.noreply.github.com>
2018-10-28 00:04:34 -0300

    add--ReservasCrud

---
+++ database/migrations/2018_10_28_024617_create_reservas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer ('fkequiamentos')->unsigned();
+            $table->foreign('fkequipamentos')->references('id')->on('equipamentos');
+            $table->date('dtagendamento');
+            $table->date('dtentrega');
+            $table->string('obs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservas');
+    }
+}
